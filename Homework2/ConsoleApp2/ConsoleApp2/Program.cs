c6c1472fe094a8f03df3cabd7a0368f66811d15c
rproph <92274984+rproph@users.noreply.github.com>
2021-10-16 16:00:59 +0300

    Add files via upload

---
+++ Homework2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class ClassA : ClassC
+    {
+        public override void DisplayC()
+        {
+            Console.WriteLine("This method was inherited from abstract class ClassC");
+        }
+        public int numberA = 10;
+        public void Display()
+        {
+            Console.WriteLine("ClassA display");
+        }
+    }
+
+    class ClassB : ClassA
+    {
+        public void Display()
+        {
+            base.Display();
+        }
+    }
+
+    abstract class ClassC
+    {
+        public abstract void DisplayC();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var overload = new Overload();
+            string line = "some line";
+            overload.Display(ref line);
+
+            ClassA a = new ClassB();
+        }
+    }
+}
