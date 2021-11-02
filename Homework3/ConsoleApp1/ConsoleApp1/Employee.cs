44b9a1c060979be476496599f201edda16188535
rproph <ilya.grebnev.212@gmail.com>
2021-11-02 20:19:03 +0300

    Update Employee.cs

--- Homework3/ConsoleApp1/ConsoleApp1/Employee.cs
+++ Homework3/ConsoleApp1/ConsoleApp1/Employee.cs
@@ -14,28 +14,37 @@ namespace ConsoleApp1
     public class Employee : User, IDisplayable
     {
         public Company Company;
+        private Random rnd = new Random();
+        private int randomArrSize
+        {
+            get
+            {
+                return randomArrSize;
+            }
+
+            set
+            {
+                randomArrSize = rnd.Next(1, 11);
+            }
+        }
 
         public void Display()
         {
             Console.WriteLine($"Hello, I am {FullName}, {JobTitle} in {Company.Name}\n({ Company.Country}, { Company.City}, { Company.Address}) and my\nsalary { JobSalary}.");
         }
 
-        public Employee CreateRndEmp()
+        public Employee CreateRandomEmployee()
         {
-            string[,] JobTitles = { { "Programmer", "Tester", "HR" }, { "Coding specialisation", "Testing specialisation", "Recruiting and training students " } };
             Random rnd = new Random();
-            Employee rndEmployee = new Employee();
-            int rndJobtitle;
-
-            rndJobtitle = rnd.Next(0, 3);
+            Employee randomEmployee = new Employee();
 
             var generator = new Faker<Employee>()
                 .StrictMode(true)
                 .RuleFor(x => x.Id, f => Guid.NewGuid())
                 .RuleFor(x => x.FirstName, f => f.Name.FirstName())
                 .RuleFor(x => x.LastName, f => f.Name.LastName())
-                .RuleFor(x => x.JobDescription, f => JobTitles[1, rndJobtitle])
-                .RuleFor(x => x.JobTitle, f => JobTitles[0, rndJobtitle])
+                .RuleFor(x => x.JobDescription, f => f.Name.JobDescriptor())
+                .RuleFor(x => x.JobTitle, f => f.Name.JobTitle())
                 .RuleFor(x => x.JobSalary, f => f.Random.Int(400, 1000))
                 .RuleFor(x => x.Company, f => null);
 
@@ -46,24 +55,10 @@ namespace ConsoleApp1
                 .RuleFor(x => x.City, f => f.Address.City())
                 .RuleFor(x => x.Address, f => f.Address.StreetAddress());
 
-            rndEmployee = generator.Generate();
-            rndEmployee.Company = companyGenerator.Generate();
+            randomEmployee = generator.Generate();
+            randomEmployee.Company = companyGenerator.Generate();
 
-            return rndEmployee;
-        }
-
-        public Employee[] RandomEmpCount()
-        {
-            Random rnd = new Random();
-            int rndArrSize = rnd.Next(1, 11);
-            Employee[] rndEmployee = new Employee[rndArrSize];
-
-            for (int i = 0; i < rndArrSize; i++)
-            {
-                rndEmployee[i] = CreateRndEmp();
-            }
-
-            return rndEmployee;
+            return randomEmployee;
         }
     }
 }
