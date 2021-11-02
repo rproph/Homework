a62953ad5a3692d473e390b624f74eff0f2287a0
rproph <ilya.grebnev.212@gmail.com>
2021-11-02 20:19:16 +0300

    Update UserFactory.cs

--- Homework3/ConsoleApp1/ConsoleApp1/UserFactory.cs
+++ Homework3/ConsoleApp1/ConsoleApp1/UserFactory.cs
@@ -7,19 +7,44 @@ namespace ConsoleApp1
         abstract public User Create();
     }
 
-    public class CandidateCreator : UserFactory
+    public class CandidateFactory : UserFactory
     {
+        public Candidate[] CreateGroupOfCandidates(int count)
+        {
+            Candidate[] randomCandidates = new Candidate[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                randomCandidates[i] = new Candidate().CreateRandomCandidate();
+            }
+
+            return randomCandidates;
+        }
+
         public override User Create()
         {
-            return new Candidate().CreateRndCand();
+            return new Candidate().CreateRandomCandidate();
         }
     }
 
-    public class EmployeeCreator : UserFactory
+    public class EmployeeFactory : UserFactory
     {
+        public Employee[] CreateGroupOfEmployees(int count)
+        {
+
+            Employee[] randomEmployee = new Employee[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                randomEmployee[i] = new Employee().CreateRandomEmployee();
+            }
+
+            return randomEmployee;
+        }
+
         public override User Create()
         {
-            return new Employee().CreateRndEmp();
+            return new Employee().CreateRandomEmployee();
         }
     }
 
