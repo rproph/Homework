e2b3f5df0e2dde971213f3516591b7251776e4f0
rproph <92274984+rproph@users.noreply.github.com>
2021-11-05 21:48:54 +0300

    Add files via upload

---
+++ Homework4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, 4, 5, 3};
+            int[] arr2 = { 1, 3, 5};
+            int[] arr = Multitudes.CreateIntersectionOfMultitudes(arr1, arr2);
+
+            foreach(int value in arr)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
