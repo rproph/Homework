cd4ccee0c26c9416a9a844f9e723f4181bb7f5b1
rproph <92274984+rproph@users.noreply.github.com>
2021-11-11 22:14:16 +0300

    Add files via upload

---
+++ Homework4/ConsoleApp1/ConsoleApp1/Classes/QueueOperation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class QueueOperation
+    {
+        public int FindMinMaxGap(Queue<int> queue)
+        {
+            int[] arr = new int[queue.Count];
+            int maxValue = queue.Peek();
+            int maxIndex = 0;
+            int minValue = queue.Peek();
+            int minIndex = 0;
+            int summ = 0;
+
+            queue.CopyTo(arr, 0);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > maxValue)
+                {
+                    maxIndex = i;
+                    maxValue = arr[i];
+                }
+                else if (arr[i] < minValue)
+                {
+                    minIndex = i;
+                    minValue = arr[i];
+                }
+            }
+
+            if (maxValue == minValue)
+            {
+                return maxValue;
+            }
+            else
+            {
+                if (maxIndex > minIndex)
+                {
+                    for (int i = minIndex; i <= maxIndex; i++)
+                    {
+                        summ += arr[i];
+                    }
+                }
+                else if (minIndex > maxIndex)
+                {
+                    for (int i = maxIndex; i <= minIndex; i++)
+                    {
+                        summ += arr[i];
+                    }
+                }
+
+                return summ;
+            }
+        }
+    }
+}
---
+++ Homework4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             //First task
+
+             int[] arr1 = { 1, 2, 3, 4, 5, 3};
+             int[] arr2 = { 1, 3, 5};
+             int[] arr = Multitudes.CreateIntersectionOfMultitudes(arr1, arr2);
+
+             foreach(int value in arr)
+             {
+                 Console.WriteLine(value);
+             }
+            
+            //Second task
+
+            Queue<int> queue = new Queue<int>(new int[]{ 1, 1, 1});
+
+            QueueOperation qOperator = new QueueOperation();
+
+            Console.WriteLine(qOperator.FindMinMaxGap(queue));
+            
+            //Third task
+
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7};
+
+            CyclicList.DoSifting(list);
+
+            // Fourth task
+
+            List<int> fourthList = new List<int>() { 1, 2, 3, 6, 5, 4 };
+
+            PancakeSorter<int> sorter = new PancakeSorter<int>();
+
+            foreach (int value in sorter.SortIntValues(fourthList))
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
