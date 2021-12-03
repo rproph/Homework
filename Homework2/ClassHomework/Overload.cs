3630536904d9dbacf4e046eba11b59278a2bbe71
rproph <ilya.grebnev.212@gmail.com>
2021-12-03 20:18:09 +0300

    add new homework2 solution

---
+++ Homework2/ClassHomework/Overload.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Overload
+    {
+        public void Display(int n)
+        {
+            Console.WriteLine($"Number is {n}");
+        }
+
+        public void Display(int n, int m)
+        {
+            Console.WriteLine($"Numbers are {n} and {m}");
+        }
+
+        public void Display(ref string line)
+        {
+            Console.WriteLine($"String is {line}");
+        }
+
+        public void Display(int n, params string[] lines)
+        {
+            Console.WriteLine($"Number is {n}");
+            foreach (string line in lines)
+            {
+                Console.WriteLine($"Line {line} in lines");
+            }
+        }
+
+    }
+}
