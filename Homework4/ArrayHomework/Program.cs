75acf5570444667f97f2f293b7c990411f8c12cf
rproph <ilya.grebnev.212@gmail.com>
2021-12-03 20:12:41 +0300

    add new homework4 solution

---
+++ Homework4/ArrayHomework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             //First task
+
+             int[] arr1 = { 1, 2, 3, 4, 5, 3};
+             int[] arr2 = { 1, 3, 5};
+             int[] arr = Multitudes.CreateIntersectionOfMultitudes(arr1, arr2);
+
+             foreach(int value in arr)
+             {
+                 Console.WriteLine(value);
+             }
+            
+
+        }
+    }
+}
