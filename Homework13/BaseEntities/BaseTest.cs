270c06e738936d526764da1394c3221efe8b9cdf
rproph <92274984+rproph@users.noreply.github.com>
2022-02-14 21:42:44 +0300

    Add files via upload

---
+++ Homework13/BaseEntities/BaseTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Homework13.BaseEntities
+{
+    public class BaseTest
+    {
+        public static string BaseURL = "https://www.saucedemo.com/";
+        [ThreadStatic] protected static IWebDriver Driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
